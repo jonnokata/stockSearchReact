e521f9f50a618c8c1b0f9d1e125aeeb146e67684
Jonno Katahanas <jonathan.katahanas@outlook.com>
2021-01-27 22:30:25 +1100

    Made routes work to add a favourite for a user and to find all favourites for a user.

--- server/src/routes/favouritesRoutes.js
+++ server/src/routes/favouritesRoutes.js
@@ -13,6 +13,9 @@ router.use((request, response, next) => {
   }
 });
 
+
+// --------------------------------------------------------
+
 //Add favourite stock to favourites list
 router.post("/new-favourite", (request, response) => {
     const requestBody = request.body;
@@ -25,37 +28,51 @@ router.post("/new-favourite", (request, response) => {
     };
 
 // Parse data from UI (stockNameConst & stockSymbolConst). How do I make requestBody not include lastClosePrice?
-FavouritesModel.create(requestBody).then((data) => {
+FavouritesModel.create(requestToDatabase).then((data) => {
     console.log(data);
     response.send(data);
+  });
 });
 
-});
+
+// --------------------------------------------------------
 
 // Find all favourites for a specific users
 router.get("/all", (req, res) => {
-  FavouritesModel.find({ userId: req.session.user.id }).then((data) => {
+  FavouritesModel.find({ userId: req.session.user.id })
+  .then((data) => {
     res.send(data);
-  })
+  });
 });
+
+
+// --------------------------------------------------------
 
 // Find if a user has favourited a specific stock
-router.get("/favourite/:symbol", (req, res) => {
+router.get("/favourite-check/:symbol", (req, res) => {
   FavouritesModel.find({ userId: req.session.user.id, stockSymbol: req.params.symbol }).then((data) => {
     res.send(data);
-  })
+  });
 });
 
 
-
+// --------------------------------------------------------
 
 // Remove favourite stock from favourites list
 
+router.delete("delete-favourite/:symbol", (request, response) => {
+  FavouritesModel.findByIdAndRemove({ userId: request.session.user.id, stockSymbol: request.params.symbol})
+  .then((data) => {
+    console.log("Unfavourite successful!");
+    response.send(data);
+  })
+  .catch(() => {
+    console.log("Something went wrong!");
+    response.status(404).send("Favourite not found");
+  });
+});
 
-
-// View favourites list (call external api for price + database for stock symbol and search)
-
-
+// --------------------------------------------------------
 
 // use findOne to check if a stock has already been favourited.
 
